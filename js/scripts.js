9ae2e37157c690d8bcd64ef8f314a8f7ea5a9936
AlexVong <avong003@gmail.com>
2021-08-30 12:33:11 -0700

    added student data in js file and linked scrip to index.html

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+
+
+var studentsSiteData = [
+	{
+		userName: 'sheriffderek',
+		siteName: 'sheriff-net',
+		siteTeaser: 'The Place to go - to see all the nare-bones assignment solutions',
+		linkText: 'Engage',
+		name: 'Derek Wood',
+		siteLink: ''
+
+	},
+
+	{
+		userName: 'ivy',
+		siteName: 'Ivy Reynolds Studio',
+		siteTeaser: '',
+		linkText: 'Enter',
+		name: 'Ivy Reynolds',
+		siteLink: '',
+	},
+
+	{
+		userName: 'alexvong',
+		siteName: `AOL (Alex On-Line)`,
+		siteTeaser: `I don't know what I will build next but I promise it won't be boring.`,
+		linkText: 'Enter AOL',
+		name: 'Alex Vong',
+		siteLink: 'https://peprojects.dev/alpha-1/alexvong/',
+	},
+
+	{
+		userName: 'dmccall',
+		siteName: 'Derek McCall',
+		siteTeaser: `Check out some of my projects`,
+		linkText: "Let's Go",
+		name: 'Derek McCall',
+		siteLink: 'https://peprojects.dev/alpha-1/dmccall/'
+
+	},
+	{
+		userName: 'tommylee',
+		siteName: `Tommy Lee's Practice Log`,
+		siteTeaser: `Cheatsheet from when I break into the tech Industry!(October)`,
+		linkText: 'Transmit',
+		name: 'Tommy Lee',
+		siteLink: 'https://peprojects.dev/alpha-1/tommylee/'
+	},
+
+	{
+		userName: 'jevohntyler',
+		siteName: `@jevohntyler`,
+		siteTeaser: `Web design for wordsmiths & worldbuilders. <br> Are you a span <span>“worldbuilder”?</span>`,
+		linkText: 'Click here to find out',
+		name: 'Jevohn Tyler',
+		siteLink: 'https://peprojects.dev/alpha-1/jevohntyler/'
+	},
+	{
+		userName: 'johnxm',
+		siteName: `JohnXM`,
+		siteTeaser: `Barista by day, Designer at night!`,
+		linkText: "Take a look at some of the stuff I'm making",
+		name: 'John Maniquis',
+		siteLink: 'https://peprojects.dev/alpha-1/johnxm/'
+	},
+
+	{
+		userName: 'Jhunt',
+		siteName: 'Concrete Jungle',
+		siteTeaser: `Dog Walker Turned Coder`,
+		linkText: "Find Out How",
+		name: 'Jesse Hunter',
+		siteLink: 'https://peprojects.dev/alpha-1/Jhunt/'
+	},
+
+
+	{
+		userName: 'mprizzuto',
+		siteName: `Marco Rizzuto`,
+		siteTeaser: `Bauhaus enthusiast and web designer`,
+		linkText: "Enter!",
+		name: 'Marco Rizzuto',
+		siteLink: 'https://peprojects.dev/alpha-1/mprizzuto/'
+	},
+];
+
+
